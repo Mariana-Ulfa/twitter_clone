f3084537c93476bb042980ed5d533c7353235eac
Mariana-Ulfa <mulfa1169@gmail.com>
2023-06-17 21:29:41 +0700

    persiapan project

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '646892fc39b1198c93e0';
+  static const String projectId = '642936c3d8d6b8d29487';
+  static const String endPoint = 'https://baas.pasarjepara.com/v1';
+}
